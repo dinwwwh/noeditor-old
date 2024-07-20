3a7b26e6e7df1d5c6638b00e856e9570c4126fce
dinwwwh <dinwwwh@gmail.com>
2024-07-20 13:02:29 +0700

    chore: lightningcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { browserslistToTargets } from 'lightningcss'
+import browserslist from 'browserslist'
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    transformer: 'lightningcss',
+    lightningcss: {
+      targets: browserslistToTargets(browserslist('>= 0.25%')),
+    },
+  },
+  build: {
+    cssMinify: 'lightningcss',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
