bcc81753439baf4db50a3cfd3dd48b6af23e2bb9
dinwwwh <dinwwwh@gmail.com>
2024-07-20 14:51:42 +0700

    feat: style link

--- src/components/plate/link-element.tsx
+++ src/components/plate/link-element.tsx
@@ -1,22 +1,18 @@
-import { cn, withRef } from '@udecode/cn'
+import { withRef } from '@udecode/cn'
 import { PlateElement, useElement } from '@udecode/plate-common'
 import { type TLinkElement, useLink } from '@udecode/plate-link'
 
 export const LinkElement = withRef<typeof PlateElement>(
-  ({ children, className, ...props }, ref) => {
+  ({ children, ...props }, ref) => {
     const element = useElement<TLinkElement>()
     const { props: linkProps } = useLink({ element })
 
     return (
       <PlateElement
-        asChild
-        className={cn(
-          'font-medium text-zinc-900 underline decoration-primary underline-offset-4 dark:text-zinc-50',
-          className,
-        )}
-        ref={ref}
         {...(linkProps as any)}
         {...props}
+        asChild
+        ref={ref}
       >
         <a>{children}</a>
       </PlateElement>
