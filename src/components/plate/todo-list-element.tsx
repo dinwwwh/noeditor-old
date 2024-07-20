ec7a7d039a0e27ead2423e13298f47bc219f1524
dinwwwh <dinwwwh@gmail.com>
2024-07-20 16:34:11 +0700

    feat: style todo list

--- src/components/plate/todo-list-element.tsx
+++ src/components/plate/todo-list-element.tsx
@@ -1,35 +1,32 @@
-import { cn, withRef } from '@udecode/cn'
+import { Checkbox, CheckboxIndicator } from '@radix-ui/react-checkbox'
+import { withRef } from '@udecode/cn'
 import { PlateElement } from '@udecode/plate-common'
 import {
   useTodoListElement,
   useTodoListElementState,
 } from '@udecode/plate-list'
-
-import { Checkbox } from './checkbox'
+import { Icons } from '../icons'
 
 export const TodoListElement = withRef<typeof PlateElement>(
-  ({ children, className, ...props }, ref) => {
+  ({ children, ...props }, ref) => {
     const { element } = props
     const state = useTodoListElementState({ element })
     const { checkboxProps } = useTodoListElement(state)
 
     return (
       <PlateElement
-        className={cn('flex flex-row py-1', className)}
-        ref={ref}
         {...props}
+        ref={ref}
+        data-checked={state.checked}
       >
-        <div
-          className="mr-1.5 flex select-none items-center justify-center"
-          contentEditable={false}
-        >
-          <Checkbox {...checkboxProps} />
-        </div>
+        <Checkbox {...checkboxProps} contentEditable={false} className="checkbox">
+          <CheckboxIndicator className="checkbox-indicator">
+            <Icons.check className="checkbox-icon" />
+          </CheckboxIndicator>
+        </Checkbox>
+
         <span
-          className={cn(
-            'flex-1 focus:outline-none',
-            state.checked && 'text-zinc-500 line-through dark:text-zinc-400',
-          )}
+          className="content"
           contentEditable={!state.readOnly}
           suppressContentEditableWarning
         >
