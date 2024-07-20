683427ecede2e9a74fab8c38cebe7d7ffaa83792
dinwwwh <dinwwwh@gmail.com>
2024-07-20 15:30:56 +0700

    feat: style hr

--- src/components/plate/hr-element.tsx
+++ src/components/plate/hr-element.tsx
@@ -1,26 +1,19 @@
-import { cn, withRef } from '@udecode/cn'
+import { withRef } from '@udecode/cn'
 import { PlateElement } from '@udecode/plate-common'
-import { useFocused, useSelected } from 'slate-react'
+import { useSelected } from 'slate-react'
 
 export const HrElement = withRef<typeof PlateElement>(
-  ({ className, nodeProps, ...props }, ref) => {
-    const { children } = props
-
+  ({ children, ...props }, ref) => {
     const selected = useSelected()
-    const focused = useFocused()
 
     return (
-      <PlateElement ref={ref} {...props}>
-        <div className="py-6" contentEditable={false}>
-          <hr
-            {...nodeProps}
-            className={cn(
-              'h-0.5 cursor-pointer rounded-sm border-none bg-zinc-100 bg-clip-content dark:bg-zinc-800',
-              selected && focused && 'ring-2 ring-zinc-950 ring-offset-2 dark:ring-zinc-300',
-              className,
-            )}
-          />
-        </div>
+      <PlateElement
+        ref={ref}
+        {...props}
+        data-selected={selected}
+      >
+        <hr />
+
         {children}
       </PlateElement>
     )
